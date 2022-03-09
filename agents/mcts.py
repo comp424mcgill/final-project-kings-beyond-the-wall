64457a8da144eec0b53b85d31c55ae09764af326
chrismartel <christian.martel2@mail.mcgill.ca>
2022-03-09 14:31:42 -0500

    removed useless method

--- agents/mcts.py
+++ agents/mcts.py
@@ -85,8 +85,6 @@ class State:
         # number of wins
         self.win_score
 
-        self.moves = self.all_possible_moves()
-
     def toggle_player(self):
         # toggle between 0 ans 1
         self.turn = (self.turn + 1) % 2
@@ -95,22 +93,13 @@ class State:
         #TODO state copy
         return
 
-
-
-    # Construct a list of all possible moves from current state.
+    # Build a list of all possible states from current state.
     # A move is represented as a tuple (x,y,dir) where x is the final
     # position in x, y is the final position on y axis and dir is the
-    # barrier direction: 'u','d','l','r'.
-    def all_possible_moves(self):
-        # TODO
-        pass
-
-    # from the list of all possible moves, build and return all possible states
+    # barrier direction: 'u','d','l','r'.    def all_possible_states(self):
     def all_possible_states(self):
+        # TODO
         states = list()
-        for move in self.moves:
-            #TODO
-            pass
         return states
 
     # Play a random move among list of possible moves on the board.
